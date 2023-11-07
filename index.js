412ce7e57c3b336d23c1e53303e7f8376814844a
674RoMaN <tauhidroman@gmail.com>
2023-11-07 11:05:20 +0600

    Purchase Post  operation done

--- index.js
+++ index.js
@@ -99,9 +99,14 @@ async function run() {
       res.send(result)
     })
     //getting purchase
-    app.get('/purchase', async (req, res) => {
-      const cursor = purchaseCollection.find()
-      const result = await cursor.toArray()
+    
+
+    app.get('/updated/:id', async(req,res) => {
+      const id=req.params.id
+      const query= {
+        _id: new ObjectId(id)
+      }
+      const result = await foodCollection.findOne(query)
       res.send(result)
     })
 
