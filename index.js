8ba7d69ff8b4839c30f650176da5e032e7cedd4e
674RoMaN <tauhidroman@gmail.com>
2023-11-09 13:22:37 +0600

    update

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ require("dotenv").config()
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors(
+//   {
+//   origin: [
+//     'http://localhost:5173'
+//   ],
+//   credentials:true
+// }
+))
 app.use(express.json())
 
 
@@ -35,15 +42,27 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    // adding Food
+    // adding token
     // app.post('/jwt', async (req, res) => {
     //   const newUser = req.body
-    //   // console.log(newUser);
-    //   // const result = await userCollection.insertOne(newUser)
-    //   // console.log(result);
-    //   res.send(newUser)
+    //   console.log( newUser);
+    //   const token = jwt.sign(newUser, process.env.secret, { expiresIn:'1h' })
+    //   res
+    //   .cookie('token',token, {
+    //     httpOnly: true,
+    //     secure:true,
+    //     // sameSite:'none'
+    //   })
+    //   .send({success: true, token})
 
     // })
+
+  //   app.post('/logout', async(req,res) => {
+  //     const user =req.body;
+  //     console.log('logging out', user);
+  //     res.clearCookie('token' , {maxAge:0}).send({success:true})
+
+  // })
     // adding Food
     app.post('/allFood', async (req, res) => {
       const newFood = req.body
